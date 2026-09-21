a228152e2095dac3ce003db24f1f5ef26629f954
1reflection <luuuuuuu_0617@qq.com>
2026-09-21 21:49:06 +0800

    Refactor test methods in CalcTest class

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -6,15 +6,14 @@ import org.junit.jupiter.api.Test;
 public class CalcTest {
   Calc c = new Calc();
 
-    @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
-    }
   @Test
-public void testSubtraction() {
-    Calc c = new Calc();
-    assertEquals(2, c.subtract(4, 2));
-}
+  void testAddition() {
+      assertEquals(4, c.add(2,2));
+  }
 
-
+  @Test
+  public void testSubtraction() {
+      Calc c = new Calc();
+      assertEquals(2, c.subtract(4, 2));
+  }
 }
